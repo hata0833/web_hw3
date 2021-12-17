be550ff98c9cf265a366ca692f0d484405dbe3ce
hata0833 <64578415+hata0833@users.noreply.github.com>
2021-12-17 23:47:00 +0800

    Initial commit

---
+++ src/main/java/com/example/web_hw3/WebHw3Application.java
@@ -0,0 +1,13 @@
+package com.example.web_hw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHw3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebHw3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web_hw3/WebHw3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_hw3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebHw3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
